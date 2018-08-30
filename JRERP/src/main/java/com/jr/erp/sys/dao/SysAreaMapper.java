32941a23328c05a17643a693022fc747eb203d04
高小虎 <@DESKTOP-S5HP2UR.lan>
2018-08-30 22:40:32 +0800

    local

--- JRERP/src/main/java/com/jr/erp/sys/dao/SysAreaMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jr.erp.sys.dao;
-
-import com.jr.erp.base.mybatis.BaseMapper;
-import com.jr.erp.sys.entity.SysArea;
-
-public interface SysAreaMapper extends BaseMapper<SysArea> {
-}
--- JRERP/src/main/java/com/jr/erp/sys/service/ISysStoreService.java
+++ JRERP/src/main/java/com/jr/erp/sys/service/ISysStoreService.java
@@ -1,22 +1,10 @@
 package com.jr.erp.sys.service;
 
-import java.util.List;
-
 import com.jr.erp.base.mybatis.BaseEntity;
 import com.jr.erp.base.mybatis.IBaseService;
-import com.jr.erp.sys.entity.SysStore;
 
 /**
  * 系统管理service
  */
 public interface ISysStoreService extends IBaseService<BaseEntity>{
-
-	/**
-	 * @return
-	 */
-	List<SysStore> selectList();
-
-	SysStore selectByString(Integer id);
-	
-	List<SysStore> selectByStringList();
 }
