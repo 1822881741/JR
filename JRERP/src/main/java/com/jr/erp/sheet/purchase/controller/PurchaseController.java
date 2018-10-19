b9017a46e5ebfc39a0e8530ae15afbd77cce299c
1822881741 <1822881741@qq.com>
2018-10-19 19:08:58 +0800

    12313

---
+++ JRERP/src/main/java/com/jr/erp/sheet/purchase/controller/PurchaseController.java
@@ -0,0 +1,18 @@
+package com.jr.erp.sheet.purchase.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/purchase")
+public class PurchaseController {    
+    
+    @RequestMapping(value = "/editBill.do")
+    public String editBill(HttpServletRequest request, Model model)
+    {
+        return "bill/purchase/editPurchaseBill";
+    }
+}    
