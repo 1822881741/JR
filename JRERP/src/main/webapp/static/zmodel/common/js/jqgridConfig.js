588e36fec5be3e1c8160817d42db2f3e08823f11
gxh <1822881741@qq.com>
2018-11-05 19:02:29 +0800

    门店

--- JRERP/src/main/webapp/static/zmodel/common/js/jqgridConfig.js
+++ JRERP/src/main/webapp/static/zmodel/common/js/jqgridConfig.js
@@ -194,3 +194,85 @@ function getJqgridClassify(type,tableDom,pagerDom) {
 		recordtext : "共{1}记录"
 	}
 }
+
+function getStoreList(tableDom,pagerDom) {
+	return config = {
+		url : 'sysStore/getStoreListData.do',
+		datatype : "json",
+		colNames : [ '操作', '系统编码', '助记码', '商品名称', '商品小类', '销售方式', '兑换', '金料', '石料', '品名', '统计大类', '统计中类', '统计小类', '标签模板', '状态' ],
+		colModel : [ {
+			name : 'opera',
+			sortable : false,formatter : function(cellValue, options, rowObject) {
+				var html="<button class='btn btn-xs btn-default' data-original-title='Edit Row' onclick=\"editCategory('" + rowObject.id + "');\"><i class='fa fa-pencil'></i></button>";
+				html += "<button class='btn btn-xs btn-default' data-original-title='Cancel' onclick=\"deleteCategory('" + rowObject.id + "');\"><i class='fa fa-times'></i></button>";
+				return html;
+			}
+		}, {
+			name : 'systemCode'
+		}, {
+			name : 'assistCode'
+		}, {
+			name : 'goodsName'
+		}, {
+			name : 'secondTypeName'
+		}, {
+			name : 'saleType',
+			formatter : function(cellValue, options, rowObject) {
+				if (cellValue == 1) {
+					return "折扣";
+				} else if (cellValue == 2) {
+					return "金价";
+				} else if (cellValue == 3) {
+					return "折扣+金价";
+				} else {
+					return "未知";
+				}
+			}
+		}, {
+			name : 'canBarter',
+			formatter : function(cellValue, options, rowObject) {
+				if (cellValue == 1) {
+					return "<span style='color:green'>可兑换</span>";
+				} else {
+					return "<span style='color:red'>不可兑换</span>";
+				}
+			}
+		}, {
+			name : 'goldName'
+		}, {
+			name : 'jewelName'
+		}, {
+			name : 'categoryName'
+		}, {
+			name : 'firstClassify'
+		}, {
+			name : 'secondClassify'
+		}, {
+			name : 'thirdClassify'
+		}, {
+			name : 'grfName'
+		}, {
+			name : 'status',
+			formatter : function(cellValue, options, rowObject) {
+				if (cellValue == 1) {
+					return "<span style='color:green'>在用</span>";
+				} else {
+					return "<span style='color:red'>禁用</span>";
+				}
+			}
+		} ],
+		caption : "门店列表详情",
+		rowNum : -1,
+		multiselect : true,
+		pager : '#'+pagerDom,
+		viewrecords : true,
+		loadonce : false,
+		autowidth : true,
+		pgbuttons : false,
+		pgtext : false,
+		rownumbers : false,
+		ondblClickRow : function(rowid) { // 双击行
+		},
+		recordtext : "共{1}记录"
+	}
+}
